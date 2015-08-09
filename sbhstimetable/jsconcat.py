55fd06e43131dda3be026e448f3d03d2e2c36c3b
keepcalm444 <keepcalm444@gmail.com>
2015-08-09 14:17:55 +1000

    jsconcat: use closure compiler to minify when called standalone.

--- sbhstimetable/jsconcat.py
+++ sbhstimetable/jsconcat.py
@@ -11,9 +11,17 @@ def concat_js(dir,out='static/belltimes.concat.js'):
     out.close()
 
 if __name__ == "__main__":
+    import yaml
     print("Concatenating script/* to static/belltimes.concat.js...")
     concat_js('script')
     print("Done!")
-
-# you can minify your own js now
-
+    with open('config.yml') as c:
+        cfg = yaml.load(c)
+    if cfg['comp']['java_exe']:
+        import closure, subprocess
+        cl_path = closure.get_jar_filename()
+        print("Minifying javascript using closure compiler at " + cl_path + "...")
+        subprocess.call([cfg['comp']['java_exe'], '-jar', cl_path, '--js', 'static/belltimes.concat.js',
+                  '--js_output_file=static/belltimes.concat.js', '--compilation_level', 'SIMPLE_OPTIMIZATIONS',
+                  '--language_in', 'ECMASCRIPT5_STRICT'])
+        print("Done!")
